7422bdf5b6cc6acc3aa5683148d2bb745d829bed
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-02-29 19:08:20 +0100

    added data analyzer which calculates stats for all data and a correlation matrix using pandas

--- be/app.py
+++ be/app.py
@@ -2,14 +2,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from services.coin_gecko_api import CoinGeckoApi
 from models.crypto_currency_data_set import CryptoCurrencyDataSet
+from services.data_analyzer import DataAnalyzer
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
 
 @app.route('/bitcoin_market_value', methods=['GET'])
@@ -17,8 +13,11 @@ def get_bitcoin_market_value():
     bitcoin_market_data = CoinGeckoApi.get_bitcoin_market_data()
     if bitcoin_market_data is not None:
         btc_data_set = CryptoCurrencyDataSet(bitcoin_market_data)
-        response = btc_data_set.to_dict()
-        return response
+        btc_data = btc_data_set.to_dict()
+        btc_analized_data = DataAnalyzer.analyze_crypto_data(btc_data)
+        btc_data.update(btc_analized_data)
+        response = btc_data
+        return jsonify(response)
     else:
         return jsonify({"error": "Failed to fetch Bitcoin market data"}), 500
 
