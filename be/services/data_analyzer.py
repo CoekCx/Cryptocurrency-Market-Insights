d28460a0463813b478fb556c93a17f9098d13c93
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-03-05 10:50:51 +0100

    added outliers data to data analysis

--- be/services/data_analyzer.py
+++ be/services/data_analyzer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class DataAnalyzer:
@@ -20,6 +21,30 @@ class DataAnalyzer:
         prices_stats = prices_df['value'].describe().to_dict()
         market_caps_stats = market_caps_df['value'].describe().to_dict()
         total_volumes_stats = total_volumes_df['value'].describe().to_dict()
+
+        # Add outliers to prices_df using Z-score method
+        threshold = 3
+
+        prices_z_scores = np.abs((prices_df['value'] - prices_df['value'].mean()) / prices_df['value'].std())
+        prices_df['is_outlier'] = prices_z_scores > threshold
+
+        market_caps_z_scores = np.abs(
+            (market_caps_df['value'] - market_caps_df['value'].mean()) / market_caps_df['value'].std())
+        market_caps_df['is_outlier'] = market_caps_z_scores > threshold
+
+        total_volumes_z_scores = np.abs(
+            (total_volumes_df['value'] - total_volumes_df['value'].mean()) / total_volumes_df['value'].std())
+        total_volumes_df['is_outlier'] = total_volumes_z_scores > threshold
+
+        # Get outliers
+        prices_outliers = prices_df[prices_df['is_outlier']]['value'].tolist()
+        market_caps_outliers = market_caps_df[market_caps_df['is_outlier']]['value'].tolist()
+        total_volumes_outliers = total_volumes_df[total_volumes_df['is_outlier']]['value'].tolist()
+
+        # Add outliers to the stats
+        prices_stats['outliers'] = prices_outliers
+        market_caps_stats['outliers'] = market_caps_outliers
+        total_volumes_stats['outliers'] = total_volumes_outliers
 
         # Correlation analysis
         correlation_matrix = {
